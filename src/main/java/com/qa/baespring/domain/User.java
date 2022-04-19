33c2de9a710b349297b4fed862e5a63302707c96
Lauren <ls_garwood@yahoo.co.uk>
2022-04-19 15:26:36 +0100

    LG: Set up, covering basics

--- src/main/java/com/qa/baespring/domain/User.java
+++ src/main/java/com/qa/baespring/domain/User.java
@@ -1,5 +1,113 @@
 package com.qa.baespring.domain;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+	// Sets this column to auto increment without having to enter the Id ourselves
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	// Properties of the class
+	// Creates a column called "first_name" or use @column to override name
+	// @Column(name = "firstname")
+	// Also use @Column to put a restraint on the column
+	@Column(nullable = false)
+	private String firstName;
+	// creates a column called "last_name"
+	private String lastName;
+	// Creates a column called "username"
+	private String username;
+
+	// Default constructor
+	public User() {
+	}
+
+	// Constructors
+	// Used for CRUD- Creating/Inserting
+	public User(String firstName, String lastName, String userName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = userName;
+	}
+
+	// Used for reading/selecting (and testing)
+	public User(long id, String firstName, String lastName, String username) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+	}
+
+	// Getters and Setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return username;
+	}
+
+	public void setUserName(String userName) {
+		this.username = userName;
+	}
+
+	// Generate toString()
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+				+ ", getId()=" + getId() + ", getFirstName()=" + getFirstName() + ", getLastName()=" + getLastName()
+				+ ", getUserName()=" + getUserName() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+	// Generate hashCode() and equals()
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, id, lastName, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(firstName, other.firstName) && id == other.id && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(username, other.username);
+	}
 
 }
