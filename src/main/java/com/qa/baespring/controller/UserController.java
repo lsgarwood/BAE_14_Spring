2f9a4b902d3c2dbaf14917a93f9151546ef9524c
Lauren <ls_garwood@yahoo.co.uk>
2022-04-20 14:03:21 +0100

    LG: added getById request

--- src/main/java/com/qa/baespring/controller/UserController.java
+++ src/main/java/com/qa/baespring/controller/UserController.java
@@ -1,16 +1,45 @@
 package com.qa.baespring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.baespring.domain.User;
+import com.qa.baespring.service.UserService;
 
 @RestController
 public class UserController {
 
+	private UserService service;
+
+	public UserController(UserService service) {
+		super();
+		this.service = service;
+	}
+
 	// Get
+	// Test with Hello string
+//	@GetMapping("/hello") // localhost:8080/hello
+//	public String helloWorld() {
+//		return "Hello World";
+//	}
+
+	// GetAll
+	// @GetMapping("/getAll") //localhost:8080/getAll
+
+	// GetByID - get one user
+	@GetMapping("/getById/{id}") // localhost:8080/getById/id
+	public User getById(@PathVariable long id) {
+		return service.getById(id);
+	}
 
 	// Post
+	// @PostMapping
 
 	// Put/Patch
+	// @PutMapping
 
 	// Delete
+	// @DeleteMapping
 
 }
--- src/main/java/com/qa/baespring/service/UserService.java
+++ src/main/java/com/qa/baespring/service/UserService.java
@@ -2,6 +2,7 @@ package com.qa.baespring.service;
 
 import org.springframework.stereotype.Service;
 
+import com.qa.baespring.domain.User;
 import com.qa.baespring.repo.UserRepo;
 
 @Service
@@ -10,9 +11,14 @@ public class UserService {
 	// make connection between the UserRepo and UserService
 	private UserRepo repo;
 
+	// create constructor
 	public UserService(UserRepo repo) {
 		super();
 		this.repo = repo;
+	}
+
+	public User getById(long id) {
+		return repo.findById(id).get(); // .get() will get user or throw no such element exception
 	}
 
 }
